2acee149021e62914d548f266d97fe5d0f5814f8
etmamate <maateus.cristo132@gmail.com>
2025-10-03 21:29:35 -0300

    fet Classe Mae PessoaAcademia, subclasses Aluno e Instrutor, criacao do metodo para mostrar informacoes do aluno :)

--- src/main/java/br/com/cardapio/restaurante/Main.java
+++ src/main/java/br/com/cardapio/restaurante/Main.java
@@ -1,6 +1,26 @@
 package br.com.cardapio.restaurante;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+import br.com.cardapio.restaurante.classes.Aluno;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        Aluno aluno = new Aluno("Mateus", "19026760760", "11/03/2004");
+
+        System.out.println(aluno.cadastroAluno());
+
+        JOptionPane.showMessageDialog(null, aluno.cadastroAluno(), "CADASTRO ALUNO!", 1);
+
+        System.out.println("Teste cadastro: ");
+
+        String novoNome = scn.nextLine();
+        aluno.setNome(novoNome);
+        System.out.println(aluno.cadastroAluno());
     }
 }
